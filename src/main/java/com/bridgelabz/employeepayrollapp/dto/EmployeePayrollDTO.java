7da1a34f451b991b7b45d3604e3d25226ab7b9ce
nk400 <navkr007@gmail.com>
2022-07-29 15:45:21 +0530

    ADDED: Rest controller Created to demonstrate various Http methods

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+
+    private String name;
+    private long salary;
+
+    public EmployeePayrollDTO(String name, long salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
