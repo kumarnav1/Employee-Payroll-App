bd0203ffc4ab380d2a3552f22b7f5258e98e2fad
nk400 <navkr007@gmail.com>
2022-07-29 23:28:24 +0530

    ADDED: Service Layer added to manage the model Responsiblity removed from controller

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.employeepayrollapp.services;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeePayrollService implements IEmployeePayrollService {
+
+    @Override
+    public List<EmployeePayrollData> getEmployeePayrollData() {
+        List<EmployeePayrollData> employeePayrollDataList = new ArrayList<EmployeePayrollData>();
+        employeePayrollDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Navneet", 7000000)));
+        return employeePayrollDataList;
+    }
+
+    @Override
+    public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+        EmployeePayrollData employeePayrollData = null;
+        employeePayrollData = new EmployeePayrollData(empId, new EmployeePayrollDTO("Navneet", 220000));
+        return employeePayrollData;
+    }
+
+    @Override
+    public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+        EmployeePayrollData employeePayrollData = null;
+        employeePayrollData = new EmployeePayrollData(1, empPayrollDTO);
+        return employeePayrollData;
+    }
+
+    @Override
+    public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
+        EmployeePayrollData employeePayrollData = null;
+        employeePayrollData = new EmployeePayrollData(empId, empPayrollDTO);
+        return employeePayrollData;
+    }
+
+    @Override
+    public void deleteEmployeePayrollData(int empId) {
+
+    }
+}
