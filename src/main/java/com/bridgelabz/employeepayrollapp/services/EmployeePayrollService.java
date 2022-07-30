23ff88b450501fb2801e738b391f29a6ff794950
nk400 <navkr007@gmail.com>
2022-07-30 21:44:34 +0530

    ADDED: Thrown User defined exception for Employee Id Not found

--- src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employeepayrollapp.services;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.exceptions.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,10 @@ public class EmployeePayrollService implements IEmployeePayrollService {
 
     @Override
     public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-        return employeePayrollList.get(empId - 1);
+        return employeePayrollList.stream()
+                .filter(employeePayrollData -> employeePayrollData.getEmployeeId() == empId)
+                .findFirst()
+                .orElseThrow(() -> new EmployeePayrollException("Employee not found In the List"));
     }
 
     @Override
